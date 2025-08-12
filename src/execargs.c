2213a1efab3630925e8247be5bdb119213262214
blazejOz <blazej.ozog@gmail.com>
2025-08-12 10:41:17 +0200

    inint and free functions

--- src/execargs.c
+++ src/execargs.c
@@ -2,3 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+typedef struct {
+    char **argv;
+    int argc;
+    int capacity;
+} ExecArgs;
+*/
+
+void execargs_init(ExecArgs *ea){
+    ea->argv = NULL;
+    ea->argc = 0;
+    ea->capacity = 0;
+}
+
+
+int execargs_push(ExecArgs *ea, const char *token) {  
+
+}
+
+int execargs_finalize(ExecArgs *ea) {             
+  
+}
+
+
+void execargs_free(ExecArgs *ea) {
+    if (ea->argv == NULL) {
+        return;
+    }
+    for (int i = 0; i < ea->argc; ++i) {
+        free(ea->argv[i]);
+    }
+    free(ea->argv);
+    ea->argv = NULL;
+    ea->argc = 0;
+    ea->capacity = 0;
+}
