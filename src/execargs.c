9d702b58e28d09fe08f7403cc1d62ba6baebe5db
blazejOz <blazej.ozog@gmail.com>
2025-09-04 10:25:57 +0200

    added memory allocation based on capacity

--- src/execargs.c
+++ src/execargs.c
@@ -10,14 +10,42 @@ typedef struct {
 } ExecArgs;
 */
 
+static int ensure_capacity(ExecArgs *ea){
+    //allocate memory
+    if (ea->capacity == 0){
+        int new_capacity = 4;
+        char **tmp = calloc(new_capacity, sizeof(*ea->argv));
+        if(tmp == NULL){
+            printf("error allocating memory");
+            return -1;
+        }   
+        ea->argv = tmp;
+        ea->capacity = new_capacity; 
+        return 0;
+    }
+    //reallocate memory when more arguments 
+    if(ea->argc == ea->capacity){
+        int new_capacity = ea->capacity * 2;
+        char **new_argv = realloc(ea->argv, new_capacity * sizeof(*ea->argv));
+        if(new_argv == NULL){
+            printf("error reallocating memory");
+            return -1;
+        }        
+        ea->argv = new_argv;
+        ea->capacity = new_capacity;
+        return 0;
+    }
+    return 0;
+}
+
 void execargs_init(ExecArgs *ea){
     ea->argv = NULL;
     ea->argc = 0;
     ea->capacity = 0;
 }
 
+int execargs_push(ExecArgs *ea, const char *token) {
 
-int execargs_push(ExecArgs *ea, const char *token) {  
 
 }
 
@@ -28,6 +56,8 @@ int execargs_finalize(ExecArgs *ea) {
 
 void execargs_free(ExecArgs *ea) {
     if (ea->argv == NULL) {
+        ea->argc = 0;
+        ea->capacity = 0;
         return;
     }
     for (int i = 0; i < ea->argc; ++i) {
