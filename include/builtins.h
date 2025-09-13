091c931e1fbca669d20f43b8f4d14865c3ffe7a4
blazejOz <blazej.ozog@gmail.com>
2025-09-13 11:59:01 +0200

    builtin frame

--- include/builtins.h
+++ include/builtins.h
@@ -1,6 +1,17 @@
 #ifndef BUILTINS_H
 #define BUILTINS_H
 
+typedef enum {
+    BUILTIN_NONE = 0,
+    BUILTIN_EXIT,
+    BUILTIN_CD,
+    BUILTIN_PWD,
+    BUILTIN_ECHO
+}BuiltinType;
 
+BuiltinType builtin_match(const char *cmd);
+
+int builtin_run(BuiltinType which, int argc, char **argv,
+    int *should_exit, int *exit_status);
 
 #endif
--- src/builtins.c
+++ src/builtins.c
@@ -1,1 +1,23 @@
 #include "builtins.h"
+#include <string.h>
+
+BuiltinType builtin_match(const char *cmd){
+
+}
+
+
+static int bi_exit(int argc, char **argv, int *should_exit, int *exit_status);
+static int bi_cd(int argc, char **argv);
+static int bi_pwd(int argc, char **argv);
+static int bi_echo(int argc, char **argv);
+
+int builtin_run(BuiltinType which, int argc, char **argv,
+                int *should_exit, int *exit_status) {
+    switch (which) {
+        case BUILTIN_EXIT: return bi_exit(argc, argv, should_exit, exit_status);
+        case BUILTIN_CD:   return bi_cd(argc, argv);
+        case BUILTIN_PWD:  return bi_pwd(argc, argv);
+        case BUILTIN_ECHO: return bi_echo(argc, argv);
+        default:           return -1;
+    }
+}
