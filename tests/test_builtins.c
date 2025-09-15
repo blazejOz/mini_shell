398549d8a6f8c379100df372f0f216d7fadc99b5
blazejOz <blazej.ozog@gmail.com>
2025-09-15 17:31:10 +0200

    TODO

---
+++ tests/test_builtins.c
@@ -0,0 +1,20 @@
+//
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include "builtins.h"
+
+
+
+void test_exit(){
+    
+
+
+}
+
+int main(void){
+    // test_exit();
+
+    puts("OK: all builtins tests passed");
+    return 0;
+}
