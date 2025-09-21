c3f5e0b9488586a5f6e4b900c94a58c30771c597
blazejOz <blazej.ozog@gmail.com>
2025-09-21 14:40:26 +0200

    imrpoved exit

--- src/builtins.c
+++ src/builtins.c
@@ -32,13 +32,19 @@ int builtin_run(BuiltinType which, int argc, char **argv,
 
 
 static int bi_exit(int argc, char **argv, int *should_exit, int *exit_status){
+    if(argc > 2){
+        fprintf(stderr, "Exit: Too many arguments");
+        return 1;
+    }
     if(argc == 1){
         *should_exit = 1;
+        *exit_status = 0;
         return 0;
     }
-    if(argc >= 2){
+    if(argc == 2){
         *should_exit = 1;
-        *exit_status = strtol(argv[1], NULL, 0);
+        int long num_exit =  strtol(argv[1], NULL, 0);
+        
         return 0;
     }
     return 1;
